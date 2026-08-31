164b5d741b70a0b6931514c95f5ca3754c0f9755
haymadre9-lab <haymadre9@gmail.com>
2026-08-31 10:29:50 +0200

    Add files via upload

--- hud2-boot.js
+++ hud2-boot.js
@@ -121,7 +121,7 @@ function boot(){
                      rain:false, spray:true, traffic:'off',
                      rbRadius:45, rbArc:18, rbSmooth:1, hud:true, horizon:0.50, carScale:1,
                      perfil:'auto', detalleCoche:true, carPhotoUrl:'', frenarCamara:false, hudScale:1,
-                     abrirAlNavegar:false, incrustado:false, destino:'#hudroad', carteles:true };
+                     abrirAlNavegar:false, incrustado:false, destino:'#hudroad', carteles:true, escala:1 };
   let cfg;
   try { cfg = Object.assign({}, HUD2_DEF, JSON.parse(localStorage.getItem(HUD2_KEY) || '{}')); }
   catch(e){ cfg = Object.assign({}, HUD2_DEF); }
@@ -527,7 +527,8 @@ function boot(){
     ['fogEnd','Niebla',60,500,10,'m',1],
     ['horizon','Encuadre vertical',30,70,1,'%',100],
     ['carScale','Tamaño del coche',60,160,1,'%',100],
-    ['hudScale','Tamaño de los textos',70,180,1,'%',100]
+    ['hudScale','Tamaño de los textos',70,180,1,'%',100],
+    ['escala','Resolución de render',35,100,5,'%',100]
   ];
   const guardar = () => { hud.set(cfg); try { localStorage.setItem(HUD2_KEY, JSON.stringify(cfg)); } catch(e){} };
   const seg = (id, opts, val) => '<div class="sg" id="'+id+'">' + opts.map(o =>
